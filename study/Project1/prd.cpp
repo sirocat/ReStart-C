e92e9199ec753bd6bd595b4f6049a91a23b79bb8
SIRO <109188471+sirocat@users.noreply.github.com>
2025-05-21 16:25:56 +0900

    .

--- study/Project1/prd.cpp
+++ study/Project1/prd.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#define SIZE 100
+#include <stdio.h> 
+#define SIZE 700
 
 int car[SIZE][SIZE];
 
@@ -10,45 +10,66 @@ void initializeCar() {
 }
 
 void drawCar() {
-    // 본체 (더 넓고 높게)
-    for (int i = 20; i < 30; i++) {
-        for (int j = 5; j < 35; j++) {
+    // 본체
+    for (int i = 220; i < 300; i++) {
+        for (int j = 50; j < 500; j++) {
             car[i][j] = 1;
         }
     }
 
-    // 윗부분 루프 (3줄만)
-    for (int i = 15; i < 20; i++) {  // 3줄로 변경
-        for (int j = 12; j < 28; j++) {
+    // 윗부분
+    for (int i = 150; i < 300; i++) {
+        for (int j = 120; j < 430; j++) {
             car[i][j] = 1;
         }
     }
 
-    // 창문 (위 루프에 비워내기)
-    for (int i = 16; i < 20; i++) {  // 창문 높이도 동일하게
-        for (int j = 14; j < 17; j++) {
+    // 창문 1, 2
+    for (int i = 160; i < 200; i++) {
+        for (int j = 180; j < 220; j++) {
             car[i][j] = 0;
         }
-        for (int j = 23; j < 26; j++) {
+        for (int j = 330; j < 370; j++) {
             car[i][j] = 0;
         }
     }
 
-    // 바퀴 (두툼하게)
-    for (int i = 30; i < 33; i++) {
-        for (int j = 10; j < 14; j++) {
+    // 바퀴
+    for (int i = 300; i < 350; i++) {
+        for (int j = 130; j < 220; j++) {
             car[i][j] = 1;
         }
-        for (int j = 26; j < 30; j++) {
+        for (int j = 330; j < 420; j++) {
             car[i][j] = 1;
+        }
+    }
+
+    // 왼쪽 바퀴 가운데 구멍
+    for (int i = 310; i < 335; i++) {
+        for (int j = 160; j < 190; j++) {
+            car[i][j] = 0;
+        }
+    }
+
+    // 오른쪽 바퀴 가운데 구멍
+    for (int i = 310; i < 335; i++) {
+        for (int j = 360; j < 390; j++) {
+            car[i][j] = 0;
         }
     }
 }
 
 void printCar() {
-    for (int i = 0; i < 40; i++) {
-        for (int j = 0; j < 40; j++) {
-            printf("%c", car[i][j] ? '0' : ' ');
+    for (int i = 0; i < 150; i++) {  // 150줄 이상 출력해보기
+        printf("\n");
+    }
+
+    for (int i = 0; i < SIZE; i += 10) {
+        for (int j = 0; j < SIZE; j += 10) {
+            if (car[i][j])
+                printf("0");
+            else
+                printf(" ");
         }
         printf("\n");
     }
